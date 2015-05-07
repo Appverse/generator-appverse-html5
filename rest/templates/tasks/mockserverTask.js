83bf2cdfe511b92bde3ab2bf2b7b83a67786a2ec
Antoine Charnoz <aecz@gft.com>
2015-05-07 18:04:30 +0200

    - Added creation of api folder if missing

--- rest/templates/tasks/mockserverTask.js
+++ rest/templates/tasks/mockserverTask.js
@@ -7,13 +7,19 @@ module.exports = function (grunt) {
         var jsonServer = require('json-server');
         var fs = require("fs");
         var path = require("path");
+
         grunt.log.writeln('Reading MOCK - JSON directory. Loading al the JSON files to the MockServer database.');
-        var p = __dirname + "/../api/";
+
+        var apiFolder = path.join(__dirname, "api");
+        if (!fs.existsSync(apiFolder)) {
+            fs.mkdirSync(apiFolder);
+        }
         var db = {};
-        fs.readdir(p, function (err, files) {
+        fs.readdir(apiFolder, function (err, files) {
+
             files.forEach(function (file) {
-                if (path.extname(p + file) === '.json') {
-                    db[path.basename(p + file, '.json')] = require(p + file);
+                if (path.extname(apiFolder + file) === '.json') {
+                    db[path.basename(apiFolder + file, '.json')] = require(apiFolder + file);
                 }
             });
         });
