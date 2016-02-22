4777e3d4948c87f351c686a460fa5154c4ba1ce2
Antoine Charnoz <antoine.charnoz@gft.com>
2016-02-22 16:33:33 +0100

    Added full crud example using ag-grid

--- generators/component/templates/crud/test/unit/$name-controllerSpec.js
+++ generators/component/templates/crud/test/unit/$name-controllerSpec.js
@@ -1,16 +1,15 @@
+/*jshint node:true */
 'use strict';
 
 ngDescribe({
     // your options
     name: 'Unit: <%=name%>',
-    modules: ['App.Controllers', 'restangular', 'ui.bootstrap'],
+    modules: ['App.Controllers', 'appverse.cache', 'appverse.rest', 'restangular', 'ui.bootstrap'],
     controllers: '<%=name%>Controller',
-    inject: ['$rootScope'],
-    tests: function (deps) {
-        it('should have a properly working <%=name%> controller', function () {
-            var scope = deps.<%=name%>Controller;
-            // deps.dController is the $scope object injected into dController
-             expect(scope.name).toEqual('<%=lodash.capitalize(name)%>');
+    tests: function(deps) {
+        it('should have a properly working <%=name%> controller', function() {
+            var scope = deps['<%=name%>Controller'];
+            expect(scope.gridOptions).not.toEqual(undefined);
         });
     }
 });
