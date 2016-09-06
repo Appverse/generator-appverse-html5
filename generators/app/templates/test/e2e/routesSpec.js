2063a2bb3e62bf929b8b32b7ccabcea38992cb15
Antoine Charnoz <antoine.charnoz@gft.com>
2016-09-06 12:37:20 +0200

    Fixed browser.get call Removed tests using routes of the demo

--- generators/app/templates/test/e2e/routesSpec.js
+++ generators/app/templates/test/e2e/routesSpec.js
@@ -1,26 +1,10 @@
 /*jshint node:true */
 'use strict';
 
-describe('E2E: Testing Routes', function () {
+describe('E2E: Testing Routes', function() {
 
-    it('should jump to the /home path when / is accessed', function () {
+    it('should jump to the /home path when / is accessed', function() {
         browser.setLocation('');
         expect(browser.getLocationAbsUrl()).toBe('/home');
     });
-
-    it('should have a working /theme route', function () {
-        browser.setLocation('theme');
-        expect(browser.getLocationAbsUrl()).toBe('/theme');
-    });
-
-    it('should have a working /components route', function () {
-        browser.setLocation('theme');
-        expect(browser.getLocationAbsUrl()).toBe('/theme');
-    });
-
-    it('should have a working /charts route', function () {
-        browser.setLocation('charts');
-        expect(browser.getLocationAbsUrl()).toBe('/charts');
-    });
-
 });
