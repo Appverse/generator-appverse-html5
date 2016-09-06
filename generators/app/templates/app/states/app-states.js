88c0e025aca38e37b29e56412ecbe706e411eb3c
Antoine Charnoz <antoine.charnoz@gft.com>
2016-09-06 18:01:21 +0200

    Move home state to home component Remove unused getStates from routeHelper Moved state otherwise to home component

--- generators/app/templates/app/states/app-states.js
+++ generators/app/templates/app/states/app-states.js
@@ -9,44 +9,6 @@
 
 angular.module('<%=appName%>App')
 
-.config(['$stateProvider', '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-
-        ///////////////////////////////
-        // 1-Redirects and Otherwise //
-        ///////////////////////////////
-
-        // Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
-        $urlRouterProvider
-
-        // The `when` method says if the url is ever the 1st param, then redirect to the 2nd param
-        // Here we are just setting up some convenience urls.
-        //                .when('/t?id', '/topics/:id')
-        //                    .when('/t/:id', '/topics/:id')
-
-
-        // If the url is ever invalid, e.g. '/asdf', then redirect to '/' aka the home state
-            .otherwise('/home');
-        //////////////////////////
-        // 2-State Configurations
-        // Several states hav been configured:
-        // home
-        // tasks
-        //
-        //////////////////////////
-        // We must configure states using $stateProvider.
-        $stateProvider
-        //////////
-        // Home //
-        //////////
-            .state('home', {
-            // Use a url of '/' to set a states as the 'index'.
-            url: '/home',
-            templateUrl: 'components/home/home.html'
-        });
-    }
-])
-
 .provider('routerHelper', routerHelperProvider);
 
 routerHelperProvider.$inject = ['$locationProvider', '$stateProvider', '$urlRouterProvider'];
@@ -64,12 +26,9 @@ function routerHelperProvider($locationProvider, $stateProvider, $urlRouterProvi
 
         var service = {
             configureStates: configureStates,
-            getStates: getStates
         };
 
         return service;
-
-        ///////////////
 
         function configureStates(states, otherwisePath) {
             states.forEach(function(state) {
@@ -79,10 +38,6 @@ function routerHelperProvider($locationProvider, $stateProvider, $urlRouterProvi
                 hasOtherwise = true;
                 $urlRouterProvider.otherwise(otherwisePath);
             }
-        }
-
-        function getStates() {
-            return $state.get();
         }
     }
 }
