51f92f9cf0e0f147558da87ce347e0962ccafcdf
Antoine Charnoz <antoine.charnoz@gft.com>
2016-09-06 20:22:42 +0200

    Fixed single quotes

--- generators/component/templates/view/test/unit/$name-controllerSpec.js
+++ generators/component/templates/view/test/unit/$name-controllerSpec.js
@@ -1,10 +1,11 @@
+/*jshint node:true */
 'use strict';
 
-describe("Unit: Testing <%=name%>", function () {
+describe('Unit: Testing <%=name%>', function() {
 
     beforeEach(angular.mock.module('App.Controllers'));
 
-    it('should have a properly working <%=name%> controller', angular.mock.inject(function ($rootScope, $controller) {
+    it('should have a properly working <%=name%> controller', angular.mock.inject(function($rootScope, $controller) {
 
         var scope = $rootScope.$new();
         $controller('<%=name%>Controller', {
